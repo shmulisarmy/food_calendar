a052bef9534c471205e81a6fa97c0ff1c85924fb
shmuli <shmulikeller@gmail.com>
2024-03-26 21:30:00 -0400

    add event button works now but there is still max display of two events per day seperate from the functionality aspect and i need to do some general clean up regarding context and choices that come with that

--- main.jsx
+++ main.jsx
@@ -1,102 +1,102 @@
-// Create User class
-class User {
-  constructor(recipes, availableIngredients, calendar) {
-    this.recipes = recipes;
-    this.availableIngredients = availableIngredients;
-    this.calendar = calendar;
-  }
+// // Create User class
+// class User {
+//   constructor(recipes, availableIngredients, calendar) {
+//     this.recipes = recipes;
+//     this.availableIngredients = availableIngredients;
+//     this.calendar = calendar;
+//   }
 
-  static canMake(recipe, copyOfIngredients) {
-    for (const reqIngredient in recipe) {
-      const reqAmount = recipe[reqIngredient];
-      if (
-        !(
-          reqIngredient in copyOfIngredients &&
-          copyOfIngredients[reqIngredient] >= reqAmount
-        )
-      ) {
-        return false;
-      }
-    }
-    return true;
-  }
+//   static canMake(recipe, copyOfIngredients) {
+//     for (const reqIngredient in recipe) {
+//       const reqAmount = recipe[reqIngredient];
+//       if (
+//         !(
+//           reqIngredient in copyOfIngredients &&
+//           copyOfIngredients[reqIngredient] >= reqAmount
+//         )
+//       ) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
 
-  static minus(recipe, copyOfIngredients) {
-    //remove from copyOfIngredients the contents of recipe
-    for (const reqIngredient in recipe) {
-      const reqAmount = recipe[reqIngredient];
-      copyOfIngredients[reqIngredient] -= reqAmount;
-    }
-  }
+//   static minus(recipe, copyOfIngredients) {
+//     //remove from copyOfIngredients the contents of recipe
+//     for (const reqIngredient in recipe) {
+//       const reqAmount = recipe[reqIngredient];
+//       copyOfIngredients[reqIngredient] -= reqAmount;
+//     }
+//   }
 
-  calculate() {
-    const copyOfIngredients = { ...this.availableIngredients };
-    const actualCalendar = [];
-    for (const day of this.calendar) {
-      actualCalendar.push({});
-      for (const dishStr of day) {
-        const dish = this.recipes[dishStr];
-        if (!User.canMake(dish, copyOfIngredients)) {
-          actualCalendar[actualCalendar.length - 1][dishStr] = "cnm";
-          continue;
-          cnm;
-        }
-        User.minus(dish, copyOfIngredients);
-        actualCalendar[actualCalendar.length - 1][dishStr] = "cm";
-      }
-    }
-    return actualCalendar;
-  }
-}
+//   calculate() {
+//     const copyOfIngredients = { ...this.availableIngredients };
+//     const actualCalendar = [];
+//     for (const day of this.calendar) {
+//       actualCalendar.push({});
+//       for (const dishStr of day) {
+//         const dish = this.recipes[dishStr];
+//         if (!User.canMake(dish, copyOfIngredients)) {
+//           actualCalendar[actualCalendar.length - 1][dishStr] = "cnm";
+//           continue;
+//           cnm;
+//         }
+//         User.minus(dish, copyOfIngredients);
+//         actualCalendar[actualCalendar.length - 1][dishStr] = "cm";
+//       }
+//     }
+//     return actualCalendar;
+//   }
+// }
 
-// Create a new instance of User
-const shmuli = new User(
-  {
-    matza: { flour: 1, water: 2 },
-    donut: { flour: 1, water: 2, sugar: 3 },
-  },
-  { flour: 30, sugar: 70, water: 80 },
-  [["matza", "donut"], ["matza", "donut"], ["matza"]]
-);
+// // Create a new instance of User
+// const shmuli = new User(
+//   {
+//     matza: { flour: 1, water: 2 },
+//     donut: { flour: 1, water: 2, sugar: 3 },
+//   },
+//   { flour: 30, sugar: 70, water: 80 },
+//   [["matza", "donut"], ["matza", "donut"], ["matza"]]
+// );
 
-function Event({ event, index, status }) {
-  return (
-    <div className={`event ${status}`}>
-      <p>{event}</p>
-    </div>
-  );
-}
+// function Event({ event, index, status }) {
+//   return (
+//     <div className={`event ${status}`}>
+//       <p>{event}</p>
+//     </div>
+//   );
+// }
 
-function Day(day) {
-  return (
-    <div className="day">
-      {Object.keys(day).map((event, index) => (
-        <Event {...{ event, index }} status = {day[event]} />
+// function Day(day) {
+//   return (
+//     <div className="day">
+//       {Object.keys(day).map((event, index) => (
+//         <Event {...{ event, index }} status = {day[event]} />
         
-      ))}
-    </div>
-  );
-}
+//       ))}
+//     </div>
+//   );
+// }
 
-function Calendar() {
-  const [data, setData] = React.useState(shmuli.calculate());
+// function Calendar() {
+//   const [data, setData] = React.useState(shmuli.calculate());
 
-  function reRender() {
-    setData(shmuli.calculate());
-  }
+//   function reRender() {
+//     setData(shmuli.calculate());
+//   }
 
-  return (
-    <div id="calendar">
-      <context.Provider value={reRender}>
-        {data.map((day, i) => (
-          <Day key={i} {...day} />
-        ))}
-      </context.Provider>
-    </div>
-  );
-}
+//   return (
+//     <div id="calendar">
+//       <context.Provider value={reRender}>
+//         {data.map((day, i) => (
+//           <Day key={i} {...day} />
+//         ))}
+//       </context.Provider>
+//     </div>
+//   );
+// }
 
-const context = React.createContext();
+// const context = React.createContext();
 
-ReactDOM.render(<Calendar />, document.getElementById("root"));
+// ReactDOM.render(<Calendar />, document.getElementById("root"));
 
